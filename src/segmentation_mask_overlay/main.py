4133fd3277e50e4f605cbc566bab059e9e4a505b
Artem Lobantsev <lobantseff@gmail.com>
2023-03-01 16:44:04 +0100

    Moved from PIL to CV (increase speed of inference)

--- src/segmentation_mask_overlay/main.py
+++ src/segmentation_mask_overlay/main.py
@@ -1,119 +1,149 @@
-import os
 from collections.abc import Iterable
 from typing import Optional, Tuple, Union, List
 
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as colormap
 from matplotlib import colors as C
 from matplotlib.patches import Patch
 from PIL import Image as PILImage
 
-from .label_color import LabelColor
-from .utils import check_convert_image
+
+def check_convert_image(image: np.ndarray) -> np.ndarray:
+    if (
+        image.dtype == np.uint8
+        and image.max() <= 255
+        and image.min() >= 0
+        and image.ndim == 3
+        and image.shape[-1] == 3
+    ):
+        return image
+
+    image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+
+    if image.ndim == 2:
+        return cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+
+    elif image.ndim == 3:
+        assert image.shape[-1] in [1, 3, 4], "Expected uint8 numpy array of shape HW1, HW3, HW4."
+        if image.shape[-1] == 1:
+            return cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+        if image.shape[-1] == 3:
+            return image
+        if image.shape[-1] == 4:
+            return cv2.cvtColor(image, cv2.COLOR_RGBA2RGB)
+
+    else:
+        raise AssertionError("Expected numpy array of shape HW, HW1, HW3, HW4.")
+
+
+def check_convert_mask(mask: np.ndarray, num_classes: int) -> np.ndarray:
+    assertion_message = "The mask is expected to be an HW array with integer per class or HWC with bool mask per channel"
+    mask = mask.astype(np.uint8)
+    if mask.ndim == 3:
+        assert mask.max() == 1, assertion_message
+        assert mask.shape[-1] == num_classes, "Num mask channels should be equal to len(labels)"
+    elif mask.ndim == 2:
+        mask = np.eye(num_classes)[mask]  # One-hot encoded array
+    else:
+        raise AssertionError(assertion_message)
+    return mask.astype(bool)
 
 
 def overlay_masks(
     image: np.ndarray,
-    boolean_masks: Union[np.ndarray, List[np.ndarray]],
-    labels: Optional[List[str]] = None,
+    masks: np.ndarray,
+    labels: List[str],
     colors: Optional[Union[np.ndarray, List[Union[str, List[float]]]]] = None,
-    figsize: Tuple[int, int] = (8, 8),
-    dpi: int = 90,
-    mask_alpha: float = 0.4,
-    mpl_colormap: str = "gist_rainbow",
-    return_pil_image: bool = False,
+    alpha: float = 1.0,
+    beta: float = 0.5,
+    return_type: str = "numpy",
+    mpl_figsize: Tuple[int, int] = (8, 8),
+    mpl_dpi: int = 90,
+    concat_original: Optional[str] = None,
 ):
     """Overlays masks on the image.
     Parameters
     ----------
     image : np.ndarray
-        Image as a numpy array.
-    boolean_masks : List[np.ndarray[bool]]
-        List of segmentation masks or numpy array of shape (height, width, n_classes).
-        All masks should be the same size, equal to size of the image.
+        Image as a numpy array of shape: HWC or HW.
+        The image will be mormalized to [0, 255] and casted to uint8 RGB. Do it manually
+        if you want to control this.
+    mask : np.ndarray[bool | int]
+        Mask should be a numpy array of shape of the image in one of the following forms:
+        - H W C, with bool mask per channel, where each channel represents a class.
+        - H W, with an pixel integer value representing a class.
     labels : Optional[List[str]], optional
-        Optional label names. Provide in the same order as the corresponding masks.
-        If not provided, will be set as range(len(boolean_masks)), by default None
+        Names of expected labels. Provide in the same order as the channels in the masks.
+        If provided, defines 
+        If not provided, will be set as range(mask.shape[-1] | max(mask) + 1), by default None
     colors : Union[np.ndarray, List[Union[str, List[float]]]], optional
         Array of shape (n_labels x 3) or list of matplotlib acceptable colornames.
         Example to get persistent colormap: `plt.cm.tab20(np.arange(NUM_LABELS))[..., :-1]`
-    figsize : tuple, optional
-        Size in inches of the output image, by default (12, 12)
-    dpi : int, optional
-        Resolution of the output image. Note: 'px, py = w * dpi, h * dpi', by default 120
-    mask_alpha : float, optional
-        Masks opaque value, by default 0.4
+    alpha : float, optional
+        Image alpha, by default 1.0
+    beta : float, optional
+        Masks alpha, by default 0.4
     mpl_colormap : str
         Matplotlib colormap name
-    return_pil_image : bool
-        If True, will return PIL image instead of matpotlib figure.
+    return_type : bool, should be numpy | pil | mpl
+        Return numpy array, or PIL image, or matpotlib figure.
+    mpl_figsize : tuple, optional
+        Size in inches of the output matpotlib figure.
+        Valid only if return_type="mpl", by default (12, 12)
+    mpl_dpi : int, optional
+        Resolution of the output matpotlib figure. Note: 'px, py = w * dpi, h * dpi'.
+        Valid only if return_type="mpl", by default 120
+    concat_original: str, optional
+        If provided, should be 'v' or 'h', represention vericat or horisontal concatenation
+        of original image to the image with overlayed masks, by default: None
 
     Returns
     -------
-    plt.Figure | PIL.Image
+    plt.Figure | PIL.Image | np.ndarray
         Output mpl figure or pillow image with masks.
     """
 
-    if isinstance(boolean_masks, np.ndarray):
-        assert (boolean_masks.ndim == 3 and boolean_masks.dtype == bool), (
-            "boolean_masks should be a list boolean numpy"
-            + " arrays or 3-dim numpy array with the last dim"
-            + " as a channel to store masks of different classes"
-        )
-        boolean_masks = [boolean_masks[:, :, i] for i in range(boolean_masks.shape[-1])]
+    assert image.shape[:2] == masks.shape[:2], "Image and mask should be of the same size"
 
-    if labels is not None:
-        assert len(labels) == len(boolean_masks), (
-            "Number of provided labels != number of masks"
-        )
-    else:
-        labels = [f"{_:02d}" for _ in range(len(boolean_masks))]
+    num_classes = len(labels)
 
     image = check_convert_image(image)
-
-    assert all(
-        mask.shape == image.shape[:2] for mask in boolean_masks
-    ), "Label mask size is not equal to image size"
+    masks = check_convert_mask(masks, num_classes)
 
     if colors is None:
-        cbar = LabelColor(
-            num_labels=len(boolean_masks),
-            alpha=mask_alpha,
-            return_legend_color=True,
-            mpl_colormap=mpl_colormap,
-            color_mode="rgb"
-        )
+        if num_classes <= 10:
+            colors = colormap.get_cmap("tab10", 10).colors[:num_classes, :-1]
+        else:
+            colors = colormap.get_cmap("rainbow_r", num_classes)(np.arange(num_classes))[:, :-1]
 
     else:
-        assert len(colors) == len(boolean_masks), (
-            "Number of provided colors != number of masks"
-        )
+        assert len(colors) == num_classes, "Number of provided colors != number of labels"
         if all(isinstance(c, str) for c in colors):
-            colors = [C.to_rgba(c) for c in colors]
+            colors = [C.to_rgb(c) for c in colors]
 
         if isinstance(colors, Iterable):
             colors = np.array(colors)
 
-        assert colors.ndim == 2 and colors.shape[-1] == 4, (
+        assert colors.ndim == 2 and colors.shape[-1] == 3, (
             "Unsupported color format:"
             + " should be list of matplotlib colorname strings for each mask/mask_channel,"
-            + " list of RGBA arrays or 2-dim numpy array of shape (n_labels x 3)"
+            + " list of RGB arrays or 2-dim numpy array of shape (n_labels x 3)"
         )
 
-        mask_colors = colors.copy()
-        # mask_colors[:, -1] *= mask_alpha
-        mask_colors = (mask_colors * 255).astype("uint8")
-        cbar = zip(mask_colors, colors)
+    mask_colors = colors.copy()
+    mask_colors = (mask_colors * 255).astype(np.uint8)
+    cbar = list(zip(mask_colors, colors))
 
-    # segmentation_overlay = np.zeros((*image_size, 4), dtype=np.uint16)
     segmentation_overlay = np.zeros_like(image, dtype=np.uint16)
     segmentation_mask = np.zeros(image.shape[:2], dtype=bool)
     legend_elements = []
 
-    for mask, label, (color, legend_color) in zip(boolean_masks, labels, cbar):
-
-        assert mask.dtype == "bool"
+    for i in range(num_classes):
+        label = labels[i]
+        mask = masks[..., i]
+        color, legend_color = cbar[i]
 
         intersection = mask & segmentation_mask
         segmentation_mask = mask | segmentation_mask
@@ -122,25 +152,33 @@ def overlay_masks(
         segmentation_overlay[mask ^ intersection] = color
 
         # Blend overlapping area
-        segmentation_overlay[intersection] = (
-            segmentation_overlay[intersection] + color
-        ) / 2
+        segmentation_overlay[intersection] = (segmentation_overlay[intersection] + color) / 2
 
         legend_elements.append(Patch(color=legend_color, label=label))
 
-    segmentation_overlay = segmentation_overlay.astype("uint8")
-    overlayed = cv2.addWeighted(image, 1, segmentation_overlay, mask_alpha, 0)
+    overlayed = cv2.addWeighted(image, alpha, segmentation_overlay.astype(np.uint8), beta, 0)
 
-    image = np.concatenate((image, overlayed), 1)
+    if concat_original is not None:
+        if concat_original == "h":
+            overlayed = np.concatenate((image, overlayed), 1)
+            mpl_figsize = (mpl_figsize[0] * 2, mpl_figsize[1])
+        elif concat_original == "v":
+            overlayed = np.concatenate((image, overlayed), 0)
+            mpl_figsize = (mpl_figsize[0], mpl_figsize[1] * 2)
+        else:
+            raise AssertionError("If provided, concat_original should be in v | h")
 
-    if return_pil_image:
-        pil_image = PILImage.fromarray(image)
+    if return_type == "numpy":
+        return overlayed
+
+    elif return_type == "pil":
+        pil_image = PILImage.fromarray(overlayed)
         return pil_image
 
-    else:
-        pil_image = PILImage.fromarray(image)
-        fig = plt.figure(figsize=figsize, dpi=dpi)
-        plt.imshow(pil_image)
+    elif return_type == "mpl":
+        fig = plt.figure(figsize=mpl_figsize, dpi=mpl_dpi)
+        plt.subplots_adjust(left=0, right=1, bottom=0, top=1, wspace=0, hspace=0)
+        plt.imshow(overlayed, vmin=0, vmax=255)
         plt.axis("off")
         mask_legend = plt.legend(
             handles=legend_elements,
@@ -151,5 +189,7 @@ def overlay_masks(
         plt.subplots_adjust(left=0.8)
         plt.tight_layout()
         plt.gca().add_artist(mask_legend)
-
         return fig
+
+    else:
+        raise AssertionError("return_type arg should be numpy | pil | mpl")
---
+++ tests/test_overlay.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from segmentation_mask_overlay import overlay_masks
+import logging
+import time
+from typing import Optional
+from contextlib import contextmanager
+
+
+@contextmanager
+def catchtime(arg: str = "", logger: Optional[logging.Logger] = None) -> float:
+    """_summary_
+
+    Returns
+    -------
+    float
+        _description_
+
+    Example
+    ------
+    >>> with catchtime() as t:
+    >>>     time.sleep(1)
+    >>> print(f"Execution time: {t():.4f} secs")
+    """
+    
+    start = time.perf_counter()
+    yield
+    if logger is not None:
+        logger.info(f"{arg} exec time: {time.perf_counter() - start:.4f} secs")
+    else:
+        print(f"{arg} exec time: {time.perf_counter() - start:.4f} secs")
+
+
+# [Example] Load image. If you are sure of you masks
+image = Image.open("./examples/cat.jpg").convert("RGB")
+image = np.array(image)
+
+image = np.random.randint(0, 1024, (256, 256)) * 255
+
+# [Example] Mimic list of masks
+masks = []
+for i in np.linspace(0, image.shape[1], 6, dtype="int"):
+    mask = np.zeros(image.shape[:2], dtype="bool")
+    mask[i : i + 100, i : i + 200] = 1
+    masks.append(mask)
+
+# [Optional] prepare labels
+mask_labels = [f"Mask_{i}" for i in range(len(masks))]
+
+# [Optional] prepare colors
+cmap = plt.cm.tab20(np.arange(len(mask_labels)))[..., :-1]
+
+# Laminate your image!
+with catchtime("numpy"):
+    array = overlay_masks(image, np.stack(masks, -1), mask_labels, return_type="numpy")
+
+with catchtime("pil"):
+    img = overlay_masks(image, np.stack(masks, -1), mask_labels, return_type="pil")
+img.save("cat_masked_pil.png")
+
+with catchtime("mpl"):
+    fig = overlay_masks(image, np.stack(masks, -1), mask_labels, return_type="mpl")
+
+fig.savefig("cat_masked.png", bbox_inches="tight")
