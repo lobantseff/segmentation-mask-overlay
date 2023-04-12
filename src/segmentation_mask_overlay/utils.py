fix uint8 normalization for uint8 images

src/segmentation_mask_overlay/utils.py
@@ -7,6 +7,10 @@ import cv2
 import numpy as np
 
 
+def normalize_to_uint8(array: np.ndarray):
+    return (array * 255 / (array.max() + 1e-6)).round().astype(np.uint8)
+
+
 def check_convert_image(image: np.ndarray, input_dims: str = "HWC") -> np.ndarray:
     if input_dims == "HWC":
         ch_dim = -1
@@ -19,12 +23,10 @@ def check_convert_image(image: np.ndarray, input_dims: str = "HWC") -> np.ndarra
         image.dtype == np.uint8
         and image.max() <= 255
         and image.min() >= 0
-        and image.ndim == 3
-        and image.shape[ch_dim] == 3
     ):
-        return image
-
-    image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+        pass
+    else:
+        image = normalize_to_uint8(image)
 
     if image.ndim == 2:
         return cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)

src/segmentation_mask_overlay/video.py
@@ -26,8 +26,13 @@ def overlay_masks_video(
     savepath: str "path/to/video.mp4"
     im_sequence : np.ndarray of shape THW or THWC
     mask_sequences : numpy arrays of shape THWC of dtype bool
+    output: str | Path
+        if output == 'numpy' output is a numpy array. If Path, an mp4 file
+        will be saved there, default: 'numpy'
+    array_dims: str, 
+        THWC | TCHW, default: THWC
     fps: int
-    array_dims: str, THWC | TCHW
+        default: 15
 
     Returns
     -------