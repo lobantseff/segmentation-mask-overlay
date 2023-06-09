:bug: fix normalization bug

src/segmentation_mask_overlay/utils.py
@@ -9,9 +9,9 @@ import numpy as np
 
 def normalize_to_uint8(array: np.ndarray):
     if array.max() == array.min():
-        array = (array * 255 / array.max() + 1e-6)
+        array = array / (array.max() + 1e-6) * 255
     else:
-        array = 255 * (array - array.min()) / (array.max() - array.min() + 1e-6)
+        array = (array - array.min()) / (array.max() - array.min() + 1e-6) * 255
     return array.round().astype(np.uint8)
 
 